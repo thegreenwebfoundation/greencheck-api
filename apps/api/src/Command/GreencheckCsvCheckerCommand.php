b672009e45e487b48aa0dbf1f0bce9f708098fc2
Chris Adams <chris@productscience.co.uk>
2019-06-11 16:50:49 +0200

    Add AJ's handy CLI management commands

---
+++ apps/api/src/Command/GreencheckCsvCheckerCommand.php
@@ -0,0 +1,61 @@
+<?php
+namespace App\Command;
+use Doctrine\ORM\EntityManagerInterface;
+use Enqueue\Client\ProducerInterface;
+use Enqueue\Util\JSON;
+use Predis\Client;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use TGWF\Greencheck\Sitecheck;
+/**
+ * GreencheckCsvCheckerCommand.
+ *
+ * Grabs all domains from a csv file in the first column and runs them through the queue
+ */
+class GreencheckCsvCheckerCommand extends Command
+{
+    /**
+     * @var ProducerInterface
+     */
+    private $producer;
+    public function __construct(
+        $name = null,
+        ProducerInterface $producer
+    ) {
+        parent::__construct($name);
+        $this->producer = $producer;
+    }
+    /**
+     * Configure the command.
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('tgwf:greencheck:csvchecker')
+            ->setDescription('Checks all domains in a csv file')
+            ;
+    }
+    /**
+     * Execute the command.
+     *
+     * @param InputInterface  $input  Input
+     * @param OutputInterface $output Output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (($handle = fopen("tgwf-check.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $domainToCheck = $data[0];
+                echo $domainToCheck;
+                $this->producer->sendCommand('greencheck', JSON::encode(['key' => 0, 'url' => $domainToCheck, 'ip' => '127.0.0.1', 'browser' => 'cli', 'source' => 'cli', 'blind' => true]), $needReply = false);
+            }
+            fclose($handle);
+        }
+        $output->writeln("");
+        $output->writeln('Check tgwf-dump.csv');
+    }
+}

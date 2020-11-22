0b57dd28ee3c916d4b7253b5d7383f1629000aa2
Chris Adams <chris@productscience.co.uk>
2020-11-22 17:43:47 +0100

    Add scripts for provisioning new box with hetzner

---
+++ ansible/install-scripts/install-server.sh
@@ -0,0 +1,15 @@
+# we need redis
+apt install redis-server --yes
+
+# we need mariadb
+apt install mariadb-server --yes
+
+# we need nginx
+apt install nginx --yes
+
+
+# we need certbot
+
+apt install git --yes
+
+
